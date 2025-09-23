cce61f6efbd78ea57113ab3c8a638e9e997b23fd
Alef Santos <alef.santos@al.infnet.edu.br>
2025-09-23 19:35:02 -0300

    style: enhance UI with improved styles for header, footer, comments, posts, and user components

--- src/components/structure/Footer.jsx
+++ src/components/structure/Footer.jsx
@@ -3,7 +3,16 @@ import styles from '../../App.module.css';
 export default function Footer() {
     return (
         <footer className={styles.rodape}>
-            Fundamentos de React
+            <p>© 2025 Alef Silva Santos</p>
+            <p>
+                <a href="https://github.com/AlefSillva" target="_blank" rel="noopener noreferrer">
+                    GitHub
+                </a>
+                {" | "}
+                <a href="https://www.linkedin.com/in/alefsilvasantos15/" target="_blank" rel="noopener noreferrer">
+                    LinkedIn
+                </a>
+            </p>
         </footer>
-    )
+    );
 }
--- src/components/userComponents/user/User.jsx
+++ src/components/userComponents/user/User.jsx
@@ -3,8 +3,16 @@ import styles from "./User.module.css";
 export default function User({ user, onClick }) {
   return (
     <div className={styles.card} onClick={() => onClick(user.id)}>
-      <h3>{user.name}</h3>
-      <p>{user.company.catchPhrase}</p>
+      <div>
+        <img 
+          src={`https://picsum.photos/seed/${user.id}/100/100`} 
+          alt="foto de perfil aleatória" 
+        />
+      </div>
+      <div className={styles.userInfo}>
+        <h3>{user.name}</h3>
+        <p>{user.company.catchPhrase}</p>
+      </div>
     </div>
   );
 }
