6d4f9d6bb17915897b81b078197732d6ddf05f7f
Alef Santos <alef.santos@al.infnet.edu.br>
2025-09-23 18:52:13 -0300

    fix: update logo image source to use imported asset

--- src/components/structure/Header.jsx
+++ src/components/structure/Header.jsx
@@ -1,10 +1,11 @@
 import styles from '../../App.module.css';
+import logo from '../../assets/logo.png';
 
 export default function Header() {
     return (
         <header className={styles.cabecalho}>
             <h1 className={styles["header-title"]}>
-                <img src="src/assets/logo.png" alt="logo" />
+                <img src={logo} alt="logo" />
             </h1>
             <h2 className={styles["header-subtitle"]}>React Social Feed</h2>
         </header>
